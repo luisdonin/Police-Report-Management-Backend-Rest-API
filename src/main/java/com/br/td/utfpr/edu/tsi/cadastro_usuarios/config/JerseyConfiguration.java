9e1419d82fb936103dd532c090c98c1518c95bd9
Donin <donin@alunos.utfpr.edu.br>
2025-09-11 16:18:40 -0300

    initialize project structure with Spring Boot, MongoDB integration, and RESTful API for user management

--- src/main/java/com/br/td/utfpr/edu/tsi/cadastro_usuarios/config/JerseyConfiguration.java
+++ src/main/java/com/br/td/utfpr/edu/tsi/cadastro_usuarios/config/JerseyConfiguration.java
@@ -1,11 +1,8 @@
 package com.br.td.utfpr.edu.tsi.cadastro_usuarios.config;
 
-
 import com.br.td.utfpr.edu.tsi.cadastro_usuarios.endpoint.UsuarioEndpoint;
 import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,5 +10,6 @@ import org.springframework.stereotype.Component;
 public class JerseyConfiguration extends ResourceConfig {
     public JerseyConfiguration() {
         this.register(UsuarioEndpoint.class);
+        this.register(CorsFilter.class);
     }
 }
